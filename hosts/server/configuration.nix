1d674c804f12416ef1426ddd27999e4af8302f73
Robert <robert.van.der.leeuw@gmail.com>
2026-03-14 01:35:36 +0100

    Added laptop.

--- hosts/server/configuration.nix
+++ hosts/server/configuration.nix
@@ -37,14 +37,17 @@
       settings = {
         gui.user = "robert";
         devices = {
-          "desktop" = {
-            id = "24PKA7Z-UWYQM46-UORQCZS-TKM3Z3F-YP2CL7Z-A5PTLI5-6EH5OKX-HZLMQAV";
-          };
+          "desktop".id = "24PKA7Z-UWYQM46-UORQCZS-TKM3Z3F-YP2CL7Z-A5PTLI5-6EH5OKX-HZLMQAV";
+          "laptop".id = "J267XXP-JPQEOP7-D23EIG5-QU25QS2-Y2KLCBT-NSLSG4A-UWDAU5X-54S4UAV";
         };
+
         folders = {
           "nc-storage" = {
             path = "/home/robert/nc";
-            devices = [ "desktop" ];
+            devices = [
+              "desktop"
+              "laptop"
+            ];
             ignorePerms = false;
           };
         };
