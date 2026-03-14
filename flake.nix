5d07e2c1db71b17dab4d2f5441d097ac9f07e50c
Robert <robert.van.der.leeuw@gmail.com>
2026-03-14 19:28:01 +0100

    Imported shell tools via dotfiles.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    dotfiles.url = "github:RobertvdLeeuw/dotfiles_nix";
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -21,7 +23,7 @@
       nixpkgs,
       home-manager,
       sops-nix,
-      # nvf,
+      dotfiles,
       ...
     }@inputs:
     {
@@ -32,22 +34,15 @@
             ./hosts/server/configuration.nix
 
             sops-nix.nixosModules.sops
-            # home-manager.nixosModules.home-manager
-            # {
-            #   home-manager = {
-            #     useGlobalPkgs = true;
-            #     useUserPackages = true;
-            #     extraSpecialArgs = {
-            #       inherit inputs;
-            #       hostType = "server";
-            #     };
-            #     users = {
-            #       robert = import ./hosts/desktop/home.nix;
-            #       root = import ./hosts/desktop/home-root.nix;
-            #     };
-            #     # sharedModules = [ nvf.homeManagerModules.default ];
-            #   };
-            # }
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                users = {
+                  robert.imports = [ dotfiles.shellEnv ];
+                  root.imports = [ dotfiles.shellEnv ];
+                };
+              };
+            }
           ];
           specialArgs = {
             inherit inputs;
