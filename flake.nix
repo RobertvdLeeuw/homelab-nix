d2209d6dcffab13815964071ecd4919a8e311420
Robert <robert.van.der.leeuw@gmail.com>
2026-03-21 00:18:01 +0100

    RPI WIP.

--- flake.nix
+++ flake.nix
@@ -22,14 +22,14 @@
     };
   };
 
-  # nixConfig = {
-  #   extra-substituters = [
-  #     "https://nixos-raspberrypi.cachix.org"
-  #   ];
-  #   extra-trusted-public-keys = [
-  #     "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
-  #   ];
-  # };
+  nixConfig = {
+    extra-substituters = [
+      "https://nixos-raspberrypi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
+    ];
+  };
 
   outputs =
     {
